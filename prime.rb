466fecacd46fbee45a6b5b3f3b49d75a58aa9032
Sean Kelly <shonkelli@gmail.com>
2019-02-04 22:19:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(num)
   num_multiples = 0
   num_array = (1..num).to_a
   num_array.each do |other_num|
-    num % other_num == 0 ? num_multiples +=1
+    num % other_num == 0 ? num_multiples +=1 :
   end
   num_multiples < 3 ? true : false
 end
