499dd33daab8431b1c6d71a8c35064e994c36bf4
Sean Kelly <shonkelli@gmail.com>
2019-02-04 22:20:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(num)
   num_multiples = 0
-  num_array = (1..num).to_a
+  num_array = (1..num.abs).to_a
   num_array.each do |other_num|
     if num % other_num == 0
       num_multiples +=1
