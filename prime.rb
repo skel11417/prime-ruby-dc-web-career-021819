a343995740adcdb0805454e5d9eff3fe6c644d96
Sean Kelly <shonkelli@gmail.com>
2019-02-04 22:23:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1 
+    return false
+  end
+  num_multiples = 0
+  num_array = (1..num).to_a
+  num_array.each do |other_num|
+    if num % other_num == 0
+      num_multiples +=1
+    end
+  end
+  num_multiples < 3 ? true : false
+end
